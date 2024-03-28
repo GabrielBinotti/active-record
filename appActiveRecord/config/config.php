1af0116d87f94490308e2963acf18aceb92262fb
Gabriel Binotti <gabrielbinotti.dev@gmail.com>
2024-03-28 16:18:31 -0300

    active-record

--- appActiveRecord/config/config.php
+++ appActiveRecord/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_HOST', '192.168.17.66');
-define('DB_NAME', 'db_netwave');
-define('DB_PORT', '3306');
-define('DB_USER', 'gabriel');
-define('DB_PASS', '@dt!63nv#pAqBi8');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_PORT', '');
+define('DB_USER', '');
+define('DB_PASS', '');
